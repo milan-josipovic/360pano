6a48a82bb8849ec5f3bda899e057640d5896fb7d
milan-josipovic <milan.josipovic@protonmail.com>
2020-08-20 01:53:23 +0200

    initial

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Home() {
+  return <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!4v1597879926931!6m8!1m7!1stpH1ibqOiqLjg3hbFNG_Pg!2m2!1d48.87356214348113!2d2.293018058297156!3f269.42287318991384!4f-16.762768380396693!5f0.7820865974627469" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  </div>
+}
