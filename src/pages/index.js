2a77deb42769009cda89bcd1cef2a57a0e851801
milan-josipovic <milan.josipovic@protonmail.com>
2020-08-20 02:04:44 +0200

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 
 export default function Home() {
   return <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!4v1597879926931!6m8!1m7!1stpH1ibqOiqLjg3hbFNG_Pg!2m2!1d48.87356214348113!2d2.293018058297156!3f269.42287318991384!4f-16.762768380396693!5f0.7820865974627469" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!4v1597879926931!6m8!1m7!1stpH1ibqOiqLjg3hbFNG_Pg!2m2!1d48.87356214348113!2d2.293018058297156!3f269.42287318991384!4f-16.762768380396693!5f0.7820865974627469" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>\
+    <iframe src="https://www.google.com/maps/embed?pb=!4v1597881788564!6m8!1m7!1sCAoSLEFGMVFpcE91TXVDSWM4aTdJdTlnX2NBZS1MYlBZV3hhcTVKd3hpZUt6UzE1!2m2!1d48.8610868022821!2d2.335605889549242!3f113.6507826222161!4f-4.819500609236783!5f0.7820865974627469" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
   </div>
 }
